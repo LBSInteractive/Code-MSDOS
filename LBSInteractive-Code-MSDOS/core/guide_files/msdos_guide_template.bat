6a0d4996d62126167917c0c2c9ebf5b1471fd8f2
LBSInteractive <36526361+LBSInteractive@users.noreply.github.com>
2018-02-27 14:22:35 -0500

    Archivo comando MSDOS
    Ejecución por CMD o .Bat windows

---
+++ LBSInteractive-Code-MSDOS/core/guide_files/msdos_guide_template.bat
@@ -0,0 +1,27 @@
+rem @echo off, permite eliminar el llamado de directorio linea por linea
+rem @echo off
+
+rem echo permite imprimir mensajes en comando
+rem echo Bienvenido al test para CPU y Memory Ram en Windows
+
+
+rem pause
+rem permite poner el sistema a espera de una accion por comando en este caso una tecla
+
+rem cls
+rem cls permite limpiar ventana de comandos
+
+
+rem start taskmgr.exe
+rem inicia un nuevo proceso en una nueva ventana
+
+
+rem :inicio 
+rem @echo off 
+rem echo Esto es un bucle infinito %username% 
+rem time /t 
+rem goto inicio
+rem Esto es un ciclo comun en MSDOS donde :inicio es el arranque hasta goto inicio para crear el bucle
+
+rem exit
+rem Salir de la ejecucion actual
